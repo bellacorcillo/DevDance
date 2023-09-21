fcbdfebf8304692fae8917bbf7dbe103b2ad7ff3
bellacorcillo <isabellacorcillo@gmail.com>
2023-09-20 17:40:24 -0700

    adding token

--- backend/routes/stats.js
+++ backend/routes/stats.js
@@ -1,15 +1,63 @@
 const express = require('express');
 const router = express.Router();
-const Stats = require('../models/stats');
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+const Stats = require('../models/Stats');
 
-// Retrieve user's stats
-router.get('/:userId', (req, res) => {
-    // Logic to get stats by userId
+// @route   POST api/stats
+// @desc    Create or Update user's stats
+// @access  Private
+router.post('/', [auth, [
+    check('pomodoros', 'Pomodoro count is required and should be an integer').isInt(),
+    check('breaks', 'Break count is required and should be an integer').isInt()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { pomodoros, breaks } = req.body;
+
+    try {
+        let stats = await Stats.findOne({ user: req.user.id });
+
+        if (!stats) {
+            // If no stats for user, create new
+            stats = new Stats({
+                user: req.user.id,
+                pomodoros,
+                breaks
+            });
+        } else {
+            // If stats exist, update the stats
+            stats.pomodoros += pomodoros;
+            stats.breaks += breaks;
+        }
+
+        await stats.save();
+        res.json(stats);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
-// Update user's stats
-router.put('/:userId', (req, res) => {
-    // Logic to update stats by userId
+// @route   GET api/stats
+// @desc    Get user's stats
+// @access  Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const stats = await Stats.findOne({ user: req.user.id });
+        if (!stats) {
+            return res.status(400).json({ msg: 'There are no stats for this user' });
+        }
+        res.json(stats);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
+
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,16 +1,38 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 
-// Register a new user
-router.post('/register', (req, res) => {
-    // Logic for user registration
+router.post('/register', [
+    check('username', 'Username is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    
+    const { username, email, password } = req.body;
+
+    try {
+        // Check if user exists
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        }
+
+        user = new User({ username, email, password });
+
+        // Encrypt password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        // Send a positive response
+        res.json({ msg: 'User registered successfully' });
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
 });
-
-// User login
-router.post('/login', (req, res) => {
-    // Logic for user login
-});
-
-module.exports = router;
-
